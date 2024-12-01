6832eca23b2e9ede8413735072a8c8f9b2b6516c
erdecapapi <erdecapapi@gmail.com>
2024-12-01 13:21:41 +0100

    Update theme in real time

--- public/js/theme-switcher.js
+++ public/js/theme-switcher.js
@@ -2,11 +2,18 @@ function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
 }
-  
+
 (function() {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        setTheme('theme-dark');
-    } else {
-        setTheme('theme-light');
+    function applyPreferedTheme() {
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            setTheme('theme-dark');
+        } else {
+            setTheme('theme-light');
+        }
     }
+
+    applyPreferedTheme();
+
+    // Listen for changes to the prefers-color-scheme media query
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', applyPreferedTheme);
 })();
