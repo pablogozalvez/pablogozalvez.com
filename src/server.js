501c8a0c8db95a3b6f520676fb865f344125d359
erdecapapi <erdecapapi@gmail.com>
2024-12-03 10:50:17 +0100

    Pequeños ajustes al servidor

--- src/server.js
+++ src/server.js
@@ -5,20 +5,22 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use((err, req, res, next) => {
-    res.status(500).json({ error: 'Internal server error' });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+
+// Aquí irán las rutas de mis apis
 
 app.use('/api/*', (req, res) => {
     res.status(404).json({ error: 'Not found' });
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, '../public/404.html'));
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).json({ error: 'Internal server error' });
 });
 
 app.listen(port, '0.0.0.0', () => {
